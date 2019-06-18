347a7f1e39f49bd61e9b424c29b6319122a121ba
huangquanfei <476001964@qq.com>
2019-06-18 10:54:20 +0800

    新增 if语句

--- src/NewTwoDaya.java
+++ src/NewTwoDaya.java
@@ -1,5 +1,9 @@
 public class NewTwoDaya {
     public static void main(String[] args) {
         System.out.println("试试");
+        int a =1;
+        if (a == 0){
+            System.out.println("滴滴滴多");
+        }
     }
 }
